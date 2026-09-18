2185ec785d2310438156f328e7834acae6943a02
Rique-Yazzie <rty7@nau.edu>
2026-09-17 19:25:48 -0700

    Initial Game Project

---
+++ new-game-project/start_screen_ui.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Yazzie_Rique_FantasyAdventure/scenes/levels/Main_Game_Level.tscn"
+	)
+
+
+func _on_quit_game_button_pressed() -> void:
+	$QuitConfirmDialog.popup_centered()
+
+
+func _on_quit_confirm_dialog_confirmed() -> void:
+	get_tree().quit()
